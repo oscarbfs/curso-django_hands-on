f375db74706a79a1945fe1d1a529009125108ddc
OscarBorges <oscar.borgesfs@gmail.com>
2021-02-05 10:39:16 -0300

    Otimizando Uploads com Collectfast, adicionado STATICFILES_STORAGE e COLLECTFAST_STRATEGY

--- pypro/settings.py
+++ pypro/settings.py
@@ -131,6 +131,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 COLLECTFAST_ENABLED = False
 
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
+
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 
 # STORAGE CONFIGURATION IN S3 AWS
