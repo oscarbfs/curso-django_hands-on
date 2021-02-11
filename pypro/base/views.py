db4585bf745b353d4448bbcfd3198ac7e0542b3f
OscarBorges <oscar.borgesfs@gmail.com>
2021-02-11 09:40:16 -0300

    Conclusão The Twelve-Factor App

--- pypro/base/views.py
+++ pypro/base/views.py
@@ -4,5 +4,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    raise ValueError()
     return HttpResponse('<html><body>Olá Django, meu nome é Oscar</body></html>', content_type='text/html')
