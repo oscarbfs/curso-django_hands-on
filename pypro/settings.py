d4bdae103d2da26724320a26a471be0d97381a51
OscarBorges <oscar.borgesfs@gmail.com>
2021-02-20 10:47:31 -0300

    Instalando Arquivos Estáticos Localmente

--- pypro/settings.py
+++ pypro/settings.py
@@ -141,7 +141,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 COLLECTFAST_ENABLED = False
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 COLLECTFAST_STRATEGY = 'collectfast.strategies.boto3.Boto3Strategy'
 
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
