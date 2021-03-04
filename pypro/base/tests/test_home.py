b9e150983a41f170d1ae0f850006565e8b995a84
OscarBorges <oscar.borgesfs@gmail.com>
2021-03-04 11:12:21 -0300

    Personalização

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -16,8 +16,8 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Python Pro</title>')
+    assert_contains(resp, '<title>OBFS</title>')
 
 
 def test_home_link(resp):
-    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+    assert_contains(resp, f'href="{reverse("base:home")}">Home</a>')
