3a667db746004782a0dc85a38e9f7b6aa1a62528
OscarBorges <oscar.borgesfs@gmail.com>
2021-03-09 09:27:10 -0300

    Ajeitado test de acordo com flake 8 e consertado posição do video

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -18,5 +18,4 @@ def test_title_video(resp):
 
 
 def test_conteudo_video(resp):
-    assert_contains(resp,
-                    '<iframe src="https://player.vimeo.com/video/519943040?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"')
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/519943040')
