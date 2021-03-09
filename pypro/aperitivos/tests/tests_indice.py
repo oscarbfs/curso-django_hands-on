013f9046f9bfa6653262086c3a520b91bc07681d
OscarBorges <oscar.borgesfs@gmail.com>
2021-03-09 11:17:41 -0300

    Lista de Elementos com For

---
+++ pypro/aperitivos/tests/tests_indice.py
@@ -0,0 +1,27 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:indice'))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+@pytest.mark.parametrize(
+    'titulo',
+    [
+        'Video Básico: Demonstração',
+        'Bugs do FIFA 21: Desinteligência artificial'
+    ]
+)
+def test_title_video(resp, titulo):
+    assert_contains(resp, titulo)
+
+
+# def test_conteudo_video(resp):
+#     assert_contains(resp, '<iframe src="https://player.vimeo.com/video/519943040')
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+videos = [
+    {'slug': 'demonstracao', 'titulo': 'Video Básico: Demonstração', 'vimeo_id': 519943040},
+    {'slug': 'desinteligencia-artificial', 'titulo': 'Bugs do FIFA 21: Desinteligência artificial',
+     'vimeo_id': 521383814}
+]
+
+videos_dct = {dct['slug']: dct for dct in videos}
+
+
+def indice(request):
+    return render(request, 'aperitivos/indice.html', context={'videos': videos})
+
 
 def video(request, slug):
-    videos = {
-        'demonstracao': {'titulo': 'Video Básico: Demonstração', 'vimeo_id': 519943040},
-        'desinteligencia-artificial': {'titulo': 'Bugs do FIFA 21: Desinteligência artificial', 'vimeo_id': 521383814}
-    }
-    video = videos[slug]
+    video = videos_dct[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
