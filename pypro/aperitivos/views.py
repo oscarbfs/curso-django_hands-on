c92e8e7f960cbfaabc5169b583388fcf3cfbb075
OscarBorges <oscar.borgesfs@gmail.com>
2021-03-20 09:16:43 -0300

    Final de Testes de Video e Criação de Migração

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,15 +2,9 @@ from django.shortcuts import render, get_object_or_404
 
 from pypro.aperitivos.models import Video
 
-videos = [
-    Video(slug='demonstracao', titulo='Video Básico: Demonstração', vimeo_id='519943040'),
-    Video(slug='desinteligencia-artificial', titulo='Bugs do FIFA 21: Desinteligência artificial', vimeo_id='521383814')
-]
-
-videos_dct = {v.slug: v for v in videos}
-
 
 def indice(request):
+    videos = Video.objects.order_by('creation').all()
     return render(request, 'aperitivos/indice.html', context={'videos': videos})
 
 
