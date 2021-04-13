2ad7b5b4cc97eece43b4b9c8f7b77a9f23bf8844
OscarBorges <oscar.borgesfs@gmail.com>
2021-04-13 16:24:59 -0300

    Admin com Modelos Relacionados

--- pypro/canais/admin.py
+++ pypro/canais/admin.py
@@ -3,8 +3,17 @@ from django.contrib import admin
 from pypro.canais.models import Canal
 
 
+class InscricaoInline(admin.TabularInline):
+    model = Canal.usuarios.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('-data',)
+
+
 @admin.register(Canal)
 class CanalAdmin(admin.ModelAdmin):
+    inlines = [InscricaoInline]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
